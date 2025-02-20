2b1d7ae41d895255801bc253629ffd8956cb8a8e
) <pratyushparida36@gmail.com>
2025-02-20 11:03:29 +0530

    retraced all files

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import LandingPg from './pages/LandingPg'
+import Page2 from './pages/Page2'
+import Skills from './pages/Skills'
+import "./index.css"
+
+const App = () => {
+  return (
+    <>
+    <div>
+      <LandingPg/>
+      <Page2 />
+      <Skills/>
+    </div>
+  </>
+  )
+}
+
+export default App
